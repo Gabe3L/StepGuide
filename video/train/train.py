81a68c79d8de36d65754603becbba66e12457468
Alex <alex@deck.co>
2025-06-21 00:42:15 -0400

    hello

---
+++ video/train/train.py
@@ -0,0 +1,28 @@
+import torch
+from ultralytics import YOLO
+
+def main():
+    best_device = 'mps' if torch.mps.is_available() else 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    model = YOLO("yolo11m.pt").half()
+
+    model.train(
+        device=best_device,
+        data="coco.yaml",
+        project="runs",
+        epochs=50,
+        imgsz=640,
+        batch=16,
+        workers=4,
+        optimizer='Adam',
+        exist_ok=True
+    )
+
+    metrics = model.val(device=best_device)
+    print(metrics)
+
+    model.export(format='onnx', half=True, device=best_device)
+    model.export(format='engine', half=True, device=best_device)
+
+if __name__ == '__main__':
+    main()
