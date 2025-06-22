663ea9cf0223223054a21e69c269ce4cc3734bc8
Gabe3L <gabe3lynch@gmail.com>
2025-06-21 20:22:49 -0400

    Modularized the main function

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import time
 from queue import Queue, Empty
 from typing import Dict, Optional, Callable
-from threading import Thread, Event
+from threading import Event
 
 import cv2
 from cv2.typing import MatLike
@@ -10,6 +10,7 @@ from cv2.typing import MatLike
 from ocr.ocr import OCR
 from tts.tts import TextToSpeech
 from video.processor import VideoProcessor
+from thread_manager import ThreadManager
 
 from logs.logging_setup import setup_logger
 
@@ -84,49 +85,7 @@ class CommandScheduler:
             self.logger.error(f"Unhandled exception in main loop: {e}")
             stop_event.set()
 
-        thread_manager.close_all_threads()   
-
-class ThreadManager:
-    def __init__(self, worker_funcs: Dict[str, Callable], logger) -> None:
-        self.logger = logger
-        self.worker_funcs = worker_funcs
-        self.threads: Dict[str, Thread] = {}
-        self.stop_event: Optional[Event] = None
-
-    def start_all_threads(self, stop_event: Event, speech_queue) -> None:
-        self.stop_event = stop_event
-        for name in ["ocr", "tts", "object_detection"]:
-            self.start_thread(name, speech_queue)
-
-    def close_all_threads(self) -> None:
-        if self.stop_event:
-            self.stop_event.set()
-
-        for name, thread in self.threads.items():
-            thread.join()
-            self.logger.info(f"Thread '{name}' joined.")
-        
-        self.threads.clear()
-        self.logger.info("All threads stopped.")
-
-    def start_thread(self, name: str, speech_queue: Queue) -> None:
-        if name in self.threads and self.threads[name].is_alive():
-            self.logger.info(f"Thread '{name}' is already running.")
-            return
-
-        if self.stop_event is None:
-            self.logger.error("Stop event not initialized.")
-            return
-
-        target = self.worker_funcs.get(name)
-        if not target:
-            self.logger.error(f"No such thread worker defined for: '{name}'")
-            return
-
-        thread = Thread(target=target, args=(self.stop_event, speech_queue), name=name)
-        thread.start()
-        self.threads[name] = thread
-        self.logger.info(f"Thread '{name}' started.")
+        thread_manager.close_all_threads()
 
 ###############################################################
 
